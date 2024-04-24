91f16576e542154b4472606263bbe28a57f7f8dd
TonySilva7 <tony.deve7oper@gmail.com>
2024-04-24 09:38:20 -0300

    feat: implements react query anc conection on api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,15 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 })
+
+// const mockDelayRequest = 2000
+
+// if (mockDelayRequest) {
+//   api.interceptors.request.use(async (config) => {
+//     await new Promise((resolve) => setTimeout(resolve, mockDelayRequest))
+
+//     return config
+//   })
+// }
