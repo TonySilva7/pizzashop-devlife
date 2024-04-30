cd8f06aee0e50ccdba2b65c71a62b7ae85ac3c1f
TonySilva7 <tony.deve7oper@gmail.com>
2024-04-30 00:13:28 -0300

    chore: config msw

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  MODE: z.enum(['production', 'development', 'test']), // "test" foi definido no package.json
   VITE_API_URL: z.string().url(),
 })
 
