9c606d0551105822a4ad907c8b9d858ec4ad4742
TonySilva7 <tony.deve7oper@gmail.com>
2024-05-01 17:31:31 -0300

    test: create mock api with msw

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   MODE: z.enum(['production', 'development', 'test']), // "test" foi definido no package.json
-  VITE_API_URL: z.string().url(),
+  VITE_API_URL: z.string(),
 })
 
 export const env = envSchema.parse(import.meta.env)
